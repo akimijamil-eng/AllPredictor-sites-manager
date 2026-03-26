c6725fe22e55d8650a9d8429caaac0fad2470374
akimijamil-eng <akimijamil@gmail.com>
2026-03-26 12:28:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs      = require('fs');
 const path    = require('path');
 
 const app  = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // ── Dossiers ──────────────────────────────────────────────
 const SITES_DIR = path.join(__dirname, 'sites');
